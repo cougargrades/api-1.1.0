67d3a4376957d7334081f427756499a96aa40fec
Austin Jackson <themacphage@gmail.com>
2020-07-06 16:51:42 -0500

    generated swagger doc

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 
@@ -14,6 +15,15 @@ const createFunction = async (
     AppModule,
     new ExpressAdapter(expressInstance),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('@cougargrades/api')
+    .setDescription('description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  console.log(document)
+  //SwaggerModule.setup('/docs', app, document);
 
   await app.init();
 };
