8abf0cf11485e7df137683539c4dc340761247ec
Austin Jackson <themacphage@gmail.com>
2020-07-06 16:02:23 -0500

    nestjs mess

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import * as functions from 'firebase-functions';
-
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ 
+import { AppModule } from './app.module';
+ 
+const expressServer = express();
+ 
+const createFunction = async (expressInstance: Express.Application): Promise<void> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+ 
+  await app.init();
+};
+ 
+export const api = functions.https.onRequest(async (request, response) => {
+  await createFunction(expressServer);
+  expressServer(request, response);
 });
+
